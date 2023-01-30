fdde879657edd8b7f0c744a234eac46234f3e546
Delia Ghita <dg277620@falmouth.ac.uk>
2023-01-30 09:59:41 +0000

    Added a money changing script
    Still needs to be attached to the player but unsure on how to work with it as a prefab

--- Assets/Scripts/UIMoneyDisplay.cs
+++ Assets/Scripts/UIMoneyDisplay.cs
@@ -23,6 +23,33 @@ public class UIMoneyDisplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Moneydisplay.text = Internalmoney.ToString();
+        Moneydisplay.text =("Money : "+ Internalmoney.ToString());
+    }
+    public void emuIncrement()
+    {
+        Internalmoney = Internalmoney + 10;
+        Moneydisplay.text = ("Money :" + Internalmoney);
+    }
+
+    // script to remove money for towers and if not enough money then it does not purchase the towers
+    public void towerDecrement(int price)
+    {
+        Internalmoney = Internalmoney - price;
+        if (Internalmoney >= 0)
+        {
+            Moneydisplay.text = ("Money :" + Internalmoney);
+        }
+        else
+        {
+            Internalmoney = Internalmoney + price;
+            Debug.Log("Not enough money");
+        }
+    }
+
+    //script to add money for the govenrment given round - might merge this and the emu increment if it ends up being the same by just getting it to pass in a value
+    public void roundIncrement()
+    {
+        Internalmoney = Internalmoney + 100;
+        Moneydisplay.text = ("Money :" + Internalmoney);
     }
 }
