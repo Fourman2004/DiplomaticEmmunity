4dadfe405bb9205e40381e4372550ca2e69231d6
Ross Michell <RM279208@falmouth.ac.uk>
2023-02-06 04:13:23 +0000

    Fullscreen/Graphics working

--- Assets/Scripts/Tower defense/optionsMenu.cs
+++ Assets/Scripts/Tower defense/optionsMenu.cs
@@ -10,4 +10,13 @@ public class optionsMenu : MonoBehaviour
     {
         audioMixer.SetFloat("Volume",volume);
     }
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
 }
+
