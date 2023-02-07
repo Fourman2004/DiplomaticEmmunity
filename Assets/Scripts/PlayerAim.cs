54af9728790ea94d1413873390d33c957d610658
MD277193 <md277193@falmouth.ac.uk>
2023-02-07 22:08:48 +0000

    fixed UI scene, player aiming works with mouse for player 1

--- Assets/Scripts/PlayerAim.cs
+++ Assets/Scripts/PlayerAim.cs
@@ -18,16 +18,27 @@ public class PlayerAim : MonoBehaviour
     {
         switch (playerNo)
         {
-            case 1:
-                KeyboardInput();
-                break;
-            case 2:
-                break;
+            //case 1:
+            //    // removed keyboard input
+            //    Debug.Log("no");
+            //    break;
+            //case 2:
+            //    break;
             default:
-                Debug.LogError("Not a valid player number.");
+                MouseInput();
                 break;
         }
     }
+
+    private void MouseInput()
+    {
+        // rotates player based on mouse position
+        var pos = Camera.main.WorldToScreenPoint(transform.position);
+        var dir = Input.mousePosition - pos;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
     private void FixedUpdate()
     {
         
