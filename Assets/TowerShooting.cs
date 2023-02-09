e06030a239dec5ba4c6a1c4b7e55d2b8f13d84bf
Ronny Ben Yehuda <rb273414@falmouth.ac.uk>
2023-02-09 11:08:10 +0000

    Tower

---
+++ Assets/TowerShooting.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerShooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletPos;
+
+    private float timer;
+    private GameObject Emu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Emu = GameObject.FindGameObjectWithTag("Emu");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector2.Distance(transform.position, Emu.transform.position);
+        Debug.Log(distance);
+
+        if(distance > 2)
+        {
+            Debug.Log(timer);
+            timer += Time.deltaTime;
+
+            if (timer > 2)
+            {
+                timer = 0;
+                shoot();
+            }
+        }
+        
+    }
+
+    void shoot()
+    {
+        GameObject Bullet = Instantiate(bullet, bulletPos.position, Quaternion.identity);
+        Bullet.AddComponent<bulletScript>();
+    }
+}
