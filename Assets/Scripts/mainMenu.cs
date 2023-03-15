4e3f1d50c1348a4e7c3f72e1b202210cb0bfcb6e
Marshall Sharp <ms279226@falmouth.ac.uk>
2023-03-15 21:05:54 +0000

    Progress

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 public class mainMenu : MonoBehaviour
 {
+    public GameObject Playbutton;
+    public GameObject OptionsButton;
+    public GameObject QuitButton;
+    int Buttonnumber;
     public void PlayButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -13,5 +18,14 @@ public class mainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void menuNav()
+    {
+       if(Input.GetAxis("Joystick 7th Axis") == 1)
+        {
+            Buttonnumber++;
+        }
+
     }
 }
