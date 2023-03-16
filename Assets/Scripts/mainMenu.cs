36d458c71f376acd4083b2301d211810afa10685
Marshall Sharp <ms279226@falmouth.ac.uk>
2023-03-16 00:49:40 +0000

    Menu selection done
    Menu selection doesn't include the options menu or tower selection menu.

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
-public class mainMenu : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
     public GameObject Playbutton;
     public GameObject OptionsButton;
     public GameObject QuitButton;
-    int Buttonnumber;
+    bool PB,OP,QB;
     public void PlayButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -20,12 +20,62 @@ public class mainMenu : MonoBehaviour
         Application.Quit();
     }
 
-    public void menuNav()
+    public void UINavigation()
     {
-       if(Input.GetAxis("Joystick 7th Axis") == 1)
+        if (PB)
         {
-            Buttonnumber++;
+            EventSystem.current.SetSelectedGameObject(Playbutton);
+            ButtonPress();
         }
-
+        else if (OP)
+        {
+            EventSystem.current.SetSelectedGameObject(OptionsButton);
+            ButtonPress();
+        }
+        else if (QB)
+        {
+            EventSystem.current.SetSelectedGameObject(QuitButton);
+            ButtonPress();
+        }
     }
+
+    public void ButtonPress()
+    {
+        if (PB && Input.GetButton("ControllerInteract"))
+        {
+            PlayButton();
+        }
+        else if(QB && Input.GetButton("ControllerInteract"))
+        {
+            QuitGame();
+        }
+    }
+
+
+    public void Update()
+    {
+
+        if (Input.GetAxisRaw("Submit") == 1)
+        {
+            PB = false;
+            OP = true;
+            QB = false;
+            UINavigation();
+        }
+        else if (Input.GetAxisRaw("Submit") == 0)
+        {
+            PB = true;
+            OP = false;
+            QB = false;
+            UINavigation();
+        }
+       else
+        {
+            PB = false;
+            OP = false;
+            QB = true;
+            UINavigation();
+        }
+    }
+
 }
