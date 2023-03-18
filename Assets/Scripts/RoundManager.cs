e56009f5327c554f7ca560752c466678867eb677
Maya Do <md277193@falmouth.ac.uk>
2023-03-18 16:43:33 +0000

    start of roundmanager and round class
    make each round interactable and accessible by roundmanager

---
+++ Assets/Scripts/RoundManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundManager : MonoBehaviour
+{
+    public int numberOfRounds = 3;
+    public int numberOfEmus;
+    public GameObject emuType;
+    private bool roundStart = true;
+    private bool roundEnd = false;
+    private int currentRound = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (roundStart == true)
+        {
+
+        }
+    }
+}
