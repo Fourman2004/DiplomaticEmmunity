e8c7d99444758423a25303cc0cbabb95c6bbfc15
Delia Ghita <dg277620@falmouth.ac.uk>
2023-03-23 10:12:37 +0000

    Added farm health script v1.0

--- Assets/Scripts/FarmHealth.cs
+++ Assets/Scripts/FarmHealth.cs
@@ -5,15 +5,36 @@ using UnityEngine.UI;
 
 public class FarmHealth : MonoBehaviour
 {
+    [SerializeField] private float maxHealth = 100;
+    [SerializeField] private float currentHealth = 0;
     public Slider slider;
+    public GameObject sliderCanvas;
+    public GameObject endText;
     public void setMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
     }
-    public void SetHealth(int health)
+    public void DammageFarm(int damage) {
+        currentHealth -= damage;
+        SetHealth(currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            EndGame();
+        }
+    }
+    public void SetHealth(float health)
     {
         slider.value = health;
+    }
+
+    public void EndGame() {
+        endText.SetActive(true);
+        sliderCanvas.SetActive(false);
+        // currently stops slider bar and appears text
+        //to do :emus stop along with time clock
     }
 
 
