15d3bcb711eaf54cc85471e9ee61bc0a0aafb83c
MAYA DO <MD277193@falmouth.ac.uk>
2023-05-02 16:54:57 +0100

    added laser shooting to tack shooter

--- Assets/Scripts/Tower/LazerFire.cs
+++ Assets/Scripts/Tower/LazerFire.cs
@@ -6,43 +6,119 @@ using UnityEngine.UI;
 
 public class LazerFire : MonoBehaviour
 {
-    public GameObject lazerpoint;
-    private LineRenderer laserLine;
+    public Transform bulletPos;
+
+    private float timer;
+    private GameObject Emu;
     private EmuTakeDamage damageScript;
-    public Transform firePoint;
-    private GameObject emu;
+
+    public float attackDamge = 10;
+    public float force = 5;
+    public float range = 10;
+
+    public Transform target;
+    private LineRenderer laserLine;
     // Start is called before the first frame update
     void Start()
     {
-        emu = GameObject.FindGameObjectWithTag("Emu");
+        laserLine = GetComponent<LineRenderer>();
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (emu == null) {
-            emu = GameObject.FindGameObjectWithTag("Emu");
-            Console.WriteLine("emu not found");
+        //Emu = GameObject.FindGameObjectWithTag("Emu");
+        //try
+        //{
+        //    float distance = Vector2.Distance(transform.position, Emu.transform.position);
+        //    if (distance < range)
+        //    {
+        //        Debug.Log("Emu in range of tower");
+        //        if (distance > 2)
+        //        {
+        //            //Debug.Log(timer);
+        //            timer += Time.deltaTime;
+
+        //            if (timer > 2)
+        //            {
+        //                timer = 0;
+        //                shoot();
+        //            }
+        //        }
+        //    }
+        //}
+        //catch (System.Exception)
+        //{
+        //    Debug.Log("No emu to hit");
+        //}
+
+        if (target == null)
+        {
+            return;
         }
-        else {
-            LazerShoot();
-            Console.WriteLine("emu found");
-        }
-        emu = GameObject.FindGameObjectWithTag("Emu");
+
     }
+    void UpdateTarget()
+    {
+        GameObject[] emus = GameObject.FindGameObjectsWithTag("Emu");
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject emu in emus)
+        {
+            float distance = Vector2.Distance(transform.position, emu.transform.position);
+            if (distance < shortestDistance)
+            {
+                shortestDistance = distance;
+                nearestEnemy = emu;
+            }
+        }
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+            //Debug.Log(timer);
+            timer += Time.deltaTime;
 
-    public void LazerShoot() {
-        // raycasting to hit the emu
-
-        RaycastHit hit;
-
-        // delay cooldown
-            
+            if (timer > 0.01)
+            {
+                timer = 0;
+                shoot(target);
+            }
+        }
+    }
+    void shoot(Transform target)
+    {
+        Vector3 temp = target.transform.position  - this.transform.position;
+        temp.Normalize();
+        LayerMask mask = LayerMask.GetMask("Emu");
+        RaycastHit2D hit2D = Physics2D.Raycast(bulletPos.transform.position, temp, range, mask);
+        laserLine.SetPosition(0, bulletPos.position); // bullet position
+        if (hit2D)
+        {
+            Debug.Log("Tack Shooter hit " + hit2D.transform.name);
+            if (hit2D.transform.tag == "Emu")
+            {
+                damageScript = hit2D.transform.gameObject.GetComponent<EmuTakeDamage>();
+                damageScript.TakeDamge(attackDamge);
+                laserLine.SetPosition(1, (Vector3)hit2D.point); // the position of emu
+            }
+        }
+        else
+        {
+            laserLine.SetPosition(1, target.transform.position /*+ new Vector3(0,0,-1)*/);
+        }
         StartCoroutine(LaserCountdown());
     }
+
     IEnumerator LaserCountdown()
     {
         yield return new WaitForSeconds(0.2f);
         laserLine.enabled = false;
+    }
+    // draws a sphere around the tower representing its range in the editor
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
